6beac7dca336809df7a601b8f88e63c578facdcb
rolandex25 <rolandex25@gmail.com>
2020-12-23 19:55:08 -0400

    Ejemplo ASPNet

---
+++ WebApplicationAspNet/WebApplicationAspNet/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplicationAspNet.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Semestre"></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" Text="Buscar" OnClick="Button1_Click" />
+            <hr>
+            <br>
+            <asp:ListBox ID="ListBox1" runat="server" Width="373px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Height="110px"></asp:ListBox>
+            <asp:Button ID="Button3" runat="server" Text="Editar" OnClick="Button3_Click" />
+            <hr>
+            <br>
+            <asp:Label ID="Label2" runat="server" Text="idMateria"></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br>
+            <asp:Label ID="Label3" runat="server" Text="sigla"></asp:Label>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br>
+            <asp:Label ID="Label4" runat="server" Text="nombre_materia"></asp:Label>
+            <asp:TextBox ID="TextBox4" runat="server" Width="321px"></asp:TextBox><br>
+            <asp:Label ID="Label5" runat="server" Text="semestre"></asp:Label>
+            <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox><br>
+            <hr>
+            <br>
+            <asp:Button ID="Button2" runat="server" Text="Guardar" OnClick="Button2_Click" />
+        
+        </div>
+        <asp:Label ID="Label6" runat="server" Text="."></asp:Label>
+        
+        <hr>
+        <asp:Label ID="Label7" runat="server" Text="Sigla"></asp:Label>
+        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox><br>
+        <asp:Label ID="Label8" runat="server" Text="Nombre materia"></asp:Label>
+        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox><br>
+        <asp:Button ID="Button4" runat="server" Text="Guardar Nuevo" OnClick="Button4_Click" />
+        <br>
+    </form>
+    
+</body>
+</html>
---
+++ WebApplicationAspNet/WebApplicationAspNet/WebForm1.aspx.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+using Npgsql;
+
+namespace WebApplicationAspNet
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label6.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+
+            String semestre = TextBox1.Text;
+            Dictionary<int, string> dMateria = new Dictionary<int, string>();
+
+
+            NpgsqlConnection con;
+            String cadena = "Server=localhost;Port=5432;User Id=postgres;Password=root;Database=academico2;";
+            try
+            {
+                con = new NpgsqlConnection(cadena);
+                con.Open();
+                String query = "select idMateria,nombre_materia" +
+                " from materia" +
+                " where semestre='" + semestre + "'";
+                NpgsqlCommand command = new NpgsqlCommand(query, con);
+
+                NpgsqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    //res = res + dr[0] + "," + dr[1] + "," + dr[2] + "," + dr[3];
+                    dMateria.Add(Convert.ToInt32(dr[0]), Convert.ToString(dr[1]));
+                }
+                con.Close();
+            }
+            catch (Exception)
+            {
+                con = null;
+            }
+            ListBox1.DataTextField = "Value";
+            ListBox1.DataValueField = "Key";
+            ListBox1.DataSource = dMateria;
+            ListBox1.DataBind();
+
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            String idMateria = ListBox1.SelectedValue.ToString();
+            Label6.Text = "";
+            NpgsqlConnection con;
+            String cadena = "Server=localhost;Port=5432;User Id=postgres;Password=root;Database=academico2;";
+            try
+            {
+                con = new NpgsqlConnection(cadena);
+                con.Open();
+                String query = "select idMateria,sigla,nombre_materia,semestre" +
+                " from materia" +
+                " where idMateria='" + idMateria + "'";
+                NpgsqlCommand command = new NpgsqlCommand(query, con);
+
+                NpgsqlDataReader dr = command.ExecuteReader();
+                while (dr.Read())
+                {
+                    //res = res + dr[0] + "," + dr[1] + "," + dr[2] + "," + dr[3];
+
+                    TextBox2.Text = Convert.ToString(dr[0]);
+                    TextBox3.Text = Convert.ToString(dr[1]);
+                    TextBox4.Text = Convert.ToString(dr[2]);
+                    TextBox5.Text = Convert.ToString(dr[3]);
+                }
+                con.Close();
+            }
+            catch (Exception)
+            {
+                con = null;
+            }
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String idMateria = TextBox2.Text;
+            String sigla = TextBox3.Text;
+            String nombre_materia = TextBox4.Text;
+            String semestre = TextBox5.Text;
+            NpgsqlConnection con;
+            String cadena = "Server=localhost;Port=5432;User Id=postgres;Password=root;Database=academico2;";
+            if (idMateria.Length > 0)
+            {
+                try
+                {
+                    con = new NpgsqlConnection(cadena);
+                    con.Open();
+                    String query = "update materia set" +
+                    " sigla='" + sigla + "', " +
+                    " nombre_materia='" + nombre_materia + "', " +
+                    " semestre='" + semestre + "'" +
+                    " where idMateria='" + idMateria + "'";
+
+                    NpgsqlCommand command = new NpgsqlCommand(query, con);
+                    command.ExecuteNonQuery();
+                    con.Close();
+
+                   Label6.Text = "Materia actualizada correctamente";
+                }
+                catch (Exception)
+                {
+                    con = null;
+
+                }
+            }
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            String idMateria = "";
+            String sigla = TextBox6.Text;
+            String nombre_materia = TextBox7.Text;
+            String semestre = TextBox1.Text;
+            NpgsqlConnection con;
+            
+
+            String cadena = "Server=localhost;Port=5432;User Id=postgres;Password=root;Database=academico2;";
+            if ((sigla.Length > 0) && (nombre_materia.Length > 0) && (semestre.Length > 0))
+            {
+                try
+                {
+                    con = new NpgsqlConnection(cadena);
+                    con.Open();
+                    String querycont = "select max(idMateria) from materia";
+                    NpgsqlCommand commandcont = new NpgsqlCommand(querycont, con);
+                    NpgsqlDataReader dr = commandcont.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        idMateria = Convert.ToString(Convert.ToInt32(dr[0])+1);
+                    }
+                    con.Close();
+
+                    Label6.Text = sigla + nombre_materia + semestre+"_"+ idMateria;
+
+                    con.Open();
+                    String query = "insert into materia values(" +
+                    "'" + idMateria + "', " +
+                    "'" + sigla + "', " +
+                    "'" + nombre_materia + "', " +
+                    "'" + semestre + "')";
+
+                    NpgsqlCommand command = new NpgsqlCommand(query, con);
+                    command.ExecuteNonQuery();
+                    con.Close();
+
+                    Label6.Text = "Materia creada correctamente";
+                }
+                catch (Exception)
+                {
+                    con = null;
+
+                }
+            }
+        }
+    }
+}
